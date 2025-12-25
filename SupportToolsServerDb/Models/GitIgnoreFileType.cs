654b54a95cc94f45fef07b9bf5a9909f3398aa28
merabza <merabza@gmail.com>
2025-12-25 07:20:48 +0400

    202512250719

--- SupportToolsServerDb/Models/GitIgnoreFileType.cs
+++ SupportToolsServerDb/Models/GitIgnoreFileType.cs
@@ -6,7 +6,7 @@ public sealed class GitIgnoreFileType
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public string? Content { get; set; }
+    public required string Content { get; set; }
 
     public ICollection<GitData> GitData { get; set; } = new List<GitData>();
 }
